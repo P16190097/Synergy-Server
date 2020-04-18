5203b907d6d9972c1a12415895a4b92d7c2cf35a
Joshua Mayo <mayoze.jm@googlemail.com>
2020-04-18 20:30:33 +0100

    fixed user unit tests, and created test database server start script

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -1,31 +1,150 @@
 import axios from 'axios';
 
 describe('User resolvers', () => {
-    test('allUsers', async () => {
+    test('createUser', async () => {
         const resp = await axios.post('http://localhost:8080/graphql', {
             query: `
-                query {
-                    allUsers {
-                        id
-                        username
-                        email
-                    }
+            mutation {
+                registerUser(username: "tester", email: "tester@test.com", password: "password") {
+                  success
+                  user {
+                    id
+                    username
+                    email
+                  }
+                  errors {
+                    path
+                    message
+                  }
                 }
+            }
             `,
         });
 
         const { data } = resp;
         expect(data).toMatchObject({
             'data': {
-                'allUsers': [
-                    {
+                'registerUser': {
+                    'success': true,
+                    'user': {
                         'id': 1,
-                        'username': 'mayoe',
-                        'email': 'mayoe@live.co.uk'
-                    }
-                ]
+                        'username': 'tester',
+                        'email': 'tester@test.com'
+                    },
+                    'errors': null
+                }
             }
         });
+    });
 
+    // test('allUsers', async () => {
+    //     const resp = await axios.post('http://localhost:8080/graphql', {
+    //         query: `
+    //             query {
+    //                 allUsers {
+    //                     id
+    //                     username
+    //                     email
+    //                 }
+    //             }
+    //         `,
+    //     });
+
+    //     const { data } = resp;
+    //     expect(data).toMatchObject({
+    //         'data': {
+    //             'allUsers': [
+    //                 {
+    //                     'id': 1,
+    //                     'username': 'tester',
+    //                     'email': 'tester@test.com'
+    //                 }
+    //             ]
+    //         }
+    //     });
+    // });
+
+    test('AuthenticateUser', async () => {
+        const resp = await axios.post('http://localhost:8080/graphql', {
+            query: `
+            mutation {
+                authenticateUser(email: "tester@test.com", password: "password") {
+                  success
+                  token
+                  refreshToken
+                  errors {
+                    path
+                    message
+                  }
+                }
+            }
+            `,
+        });
+
+        const { data } = resp;
+        expect(data).toMatchObject({
+            'data': {
+                'authenticateUser': {
+                    'success': true,
+                    'token': expect.any(String),
+                    'refreshToken': expect.any(String),
+                    'errors': null
+                }
+            }
+        });
+    });
+
+    test('DeleteUser', async () => {
+        const auth = await axios.post('http://localhost:8080/graphql', {
+            query: `
+            mutation {
+                authenticateUser(email: "tester@test.com", password: "password") {
+                  success
+                  token
+                  refreshToken
+                  errors {
+                    path
+                    message
+                  }
+                }
+            }
+            `,
+        });
+
+        const { data: { authenticateUser: { token, refreshToken } } } = auth.data;
+
+        console.log(token, refreshToken);
+
+        const resp = await axios.post('http://localhost:8080/graphql',
+            {
+                query: `
+                mutation {
+                    deleteUser {
+                        success
+                        errors {
+                            path
+                            message
+                        }
+                    }
+
+                }
+            `,
+            },
+            {
+                headers: {
+                    'x-token': token,
+                    'x-refresh-token': refreshToken,
+                },
+            }
+        );
+        const { data } = resp;
+        expect(data).toMatchObject({
+            'data': {
+                'deleteUser': {
+                    'success': true,
+                    'errors': null
+                }
+            }
+        });
     });
 });
